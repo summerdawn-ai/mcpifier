c988689559eb48b65426e69f95e15568391bb0fc
Björn Jarisch <me@singularitopian.com>
2025-12-15 18:17:20 +0100

    Add commmand-line server "mcpify.exe" for both http and stdio
    - Use System.CommandLine for argument parsing.
    - Use config for Summerdawn REST API as example.

---
+++ src/Summerdawn.Mcpify.Server/Program.cs
@@ -0,0 +1,79 @@
+using System.CommandLine;
+
+using Summerdawn.Mcpify.DependencyInjection;
+
+namespace Summerdawn.Mcpify.Server;
+
+public  class Program
+{
+    public static int Main(string[] args)
+    {
+        var modeOption = new Option<string>("--mode", "The server mode to use")
+        {
+            IsRequired = true
+        };
+        modeOption.AddAlias("-m");
+        modeOption.FromAmong("http", "stdio");
+
+        var rootCommand = new RootCommand("MCP server that can run in HTTP or stdio mode");
+        rootCommand.AddOption(modeOption);
+        rootCommand.SetHandler(mode => MainWithMode(args, mode), modeOption);
+
+        return rootCommand.Invoke(args);
+    }
+
+    private static void MainWithMode(string[] args, string mode)
+    {
+        if (mode == "http")
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Load tool mappings from separate file.
+            // Set DOTNET_CONTENTROOT environment variable if the file is _not_ in the current working directory.
+            builder.Configuration.AddJsonFile("mappings.json", optional: false, reloadOnChange: true);
+
+            // Configure HTTP MCP proxy.
+            builder.Services.AddMcpify(builder.Configuration.GetSection("Mcpify")).AddAspNetCore();
+
+            // Configure CORS to allow any connection.
+            builder.Services.AddCors(cors => cors.AddDefaultPolicy(policy =>
+                policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+
+            // Use HTTP MCP proxy.
+            app.MapMcpify();
+
+            // Use CORS.
+            app.UseCors();
+
+            app.Run();
+        }
+        else
+        {
+            var builder = Host.CreateApplicationBuilder(args);
+
+            // Load tool mappings from separate file.
+            // Set DOTNET_CONTENTROOT environment variable if the file is _not_ in the current working directory.
+            builder.Configuration.AddJsonFile("mappings.json", optional: false, reloadOnChange: true);
+
+            // Configure stdio MCP proxy.
+            builder.Services.AddMcpify(builder.Configuration.GetSection("Mcpify"));
+
+            // Send all console logging output to stderr so that it doesn't interfere with MCP stdio traffic.
+            builder.Logging.AddConsole(options =>
+            {
+                options.LogToStandardErrorThreshold = LogLevel.Trace;
+            });
+
+            var app = builder.Build();
+
+            // Use stdio MCP proxy.
+            app.UseMcpify();
+
+            app.Run();
+        }
+    }
+}
